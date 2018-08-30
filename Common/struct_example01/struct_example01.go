1c9e047acdf40b7aa608244766f6507fdb7ef454
u1095279159872 <1095279159872@tianchi.com>
2018-08-30 17:29:54 +0800

    add scdsa

---
+++ Common/struct_example01/struct_example01.go
@@ -0,0 +1,15 @@
+package main
+
+type SmartContract struct {
+}
+
+type Car struct {
+	Make   string `json:"make"`
+	Model  string `json:"model"`
+	Colour string `json:"colour"`
+	Owner  string `json:"owner"`
+}
+
+func main() {
+
+}
